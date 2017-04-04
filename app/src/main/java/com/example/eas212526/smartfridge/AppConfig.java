9fdfb2b67780a5ba182e0d0966f25f889320f32f
Superernest8 <des.ern8@gmail.com>
2017-04-04 21:58:54 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/eas212526/smartfridge/AppConfig.java
@@ -0,0 +1,12 @@
+package com.example.eas212526.smartfridge;
+
+/**
+ * Created by DELL on 04-04-2017.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.0.103/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.0.103/android_login_api/register.php";
+}
